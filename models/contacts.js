29e358e68c910f6e8f7a8de872e075f64d7354e1
H4Y6 <h4y6@i.ua>
2023-02-11 17:48:40 +0200

    init ht-02a

--- models/contacts.js
+++ models/contacts.js
@@ -2,7 +2,7 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, "db/contacts.json");
+const contactsPath = path.join(__dirname, "contacts.json");
 
 async function listContacts() {
   const contacts = await fs.readFile(contactsPath);
@@ -39,8 +39,26 @@ async function addContact(name, email, phone) {
   return newContact;
 }
 
+async function updateContact(contactId, name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { name, email, phone };
+  const idx = contacts.find((c) => c.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  contacts[idx] = newContact;
+  await updateContacts(contacts);
+  return contacts[idx];
+}
+
 async function updateContacts(contacts) {
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 }
 
-module.exports = { listContacts, getContactById, removeContact, addContact };
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
---
+++ routes/api/contacts.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const contacts = require("../../models/contacts");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
