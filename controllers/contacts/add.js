348801dd33fe1e816dbf27998bda909c7226a80c
h4y6 <h4y6@i.ua>
2023-04-20 18:33:54 +0300

    new files: middlewares/auth.js, middlewares/index.js 	modified: routes/api/contacts.js, controllers/contacts/add.js

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,14 +1,14 @@
-const { Contact, schemas } = require("../../models/contacts");
-const { createError } = require("../../helpers");
+const { basedir } = global;
+const { Contact, schemas } = require(`${basedir}/models/contacts`);
+const { createError } = require(`${basedir}/helpers`);
 
 const add = async (req, res, next) => {
   const { error } = schemas.addSchema.validate(req.body);
   if (error) {
-    // const err = new Error(error.message);
-    // throw err;
     throw createError(400, error.message);
   }
-  const result = await Contact.create(req.body);
+  const { id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
