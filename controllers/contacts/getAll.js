97abe224d229a390f731bc7043d3dda9aa32e022
h4y6 <h4y6@i.ua>
2023-04-20 19:41:53 +0300

    modified:   controllers/contacts/getAll.js

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,10 +1,12 @@
 const { Contact } = require("../../models/contacts");
 const getAll = async (req, res, next) => {
   const { id: owner } = req.user;
-  const result = await Contact.find(
-    { owner },
-    "-createdAt -updatedAt "
-  ).populate("owner", "email subscription");
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt ", {
+    skip,
+    limit: 2,
+  }).populate("owner", "email subscription");
   res.json(result);
 };
 
