a935ebf91e9bc5e6b7c6c308404303e5a6115dc2
h4y6 <h4y6@i.ua>
2023-04-29 19:11:08 +0300

    modified:   middlewares/manipulateImg.js

--- middlewares/manipulateImg.js
+++ middlewares/manipulateImg.js
@@ -1,10 +1,14 @@
 const Jimp = require("jimp");
 
-const manipulateImg = async (tmpPath) => {
-  const avatar = await Jimp.read(tmpPath);
-  avatar.resize(250, 250); // resize
-  avatar.quality(77);
-  avatar.write(tmpPath);
-};
+async function manipulateImg(tmpPath) {
+  try {
+    const avatar = await Jimp.read(tmpPath);
+    avatar.resize(250, 250); // resize
+    avatar.quality(77);
+    avatar.write(tmpPath);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 module.exports = manipulateImg;
